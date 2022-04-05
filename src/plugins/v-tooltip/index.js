cfbd89e29a96e93cb1f2bae0bcac6aa2183fd3a6
Antoine ZANARDI <antoine.zanardi@daveo.fr>
2022-04-05 22:33:48 +0200

    feat(TurnPlaying): First version for card helper

--- src/plugins/v-tooltip/index.js
+++ src/plugins/v-tooltip/index.js
@@ -1,5 +1,13 @@
 import VTooltipPlugin from "v-tooltip";
 
 export function setupVTooltip(app) {
-    app.use(VTooltipPlugin, { themes: { tooltip: { delay: { show: 0 } } } });
+    app.use(VTooltipPlugin, {
+        themes: {
+            "tooltip": { delay: { show: 0 } },
+            "card-helper-tooltip": {
+                $extend: "tooltip",
+                delay: { show: 0, hide: 0 },
+            },
+        },
+    });
 }
